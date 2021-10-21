600636829fd647d61c009acf26cc33adc4bcf792
Tree <56370416+Tojo-hung@users.noreply.github.com>
2021-10-20 21:10:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,48 @@
 import cv2
 import os
 
-dir = # folder with images
+dir = "C:/Users/xrace/Documents/Pixel"
 
 list = os.listdir(dir) # dir is your directory path
 number_image = len(list)
 
 for i in range(number_image):
- 
-  image_name = "%d.png"%i
+  i = i+1
+
+  image_name = "C:/Users/xrace/Documents/Pixel/image%d.png"%i
   
   input = cv2.imread(image_name)
 
   # Get input size
   height, width = input.shape[:2]
 
-  # Desired "pixelated" size
-  w, h = (x, 16)
+  g = 1
+  while g < 5:
+    if g == 1:
+      x = 20
+    elif g ==2:
+      x = 10
+    elif g == 3:
+      x = 5
+    elif g == 4 :
+      x = 2 
 
-# Resize input to "pixelated" size
-temp = cv2.resize(input, (w, h), interpolation=cv2.INTER_LINEAR)
+    z = round(x/width, 2)
+    
+    y = int(height * z)
+    print(y)
 
-# Initialize output image
-output = cv2.resize(temp, (width, height), interpolation=cv2.INTER_NEAREST)
+   
 
-cv2.imshow('Input', input)
-cv2.imshow('Output', output)
+    # Resize input to "pixelated" size
+    temp = cv2.resize(input, (x, y), interpolation=cv2.INTER_LINEAR)
 
-cv2.waitKey(0)
+    # Initialize output image
+    output = cv2.resize(temp, (width, height), interpolation=cv2.INTER_NEAREST)
+
+    cv2.imshow('Input', input)
+    cv2.imshow('Output', output)
+
+    k = cv2.waitKey(5000) & 0xff
+    cv2.destroyAllWindows()
+    g += 1
