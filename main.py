110e78f22a48ae92cedb1490f94854d8c1bb3edf
Tree <56370416+Tojo-hung@users.noreply.github.com>
2021-11-16 22:30:55 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 import os
 
-dir = "C:/Users/xrace/Documents/Pixel"
+dir = "C:/Users/xrace/Desktop/Pack 1 -- Portraits"
 
 list = os.listdir(dir) # dir is your directory path
 number_image = len(list)
@@ -9,41 +9,44 @@ number_image = len(list)
 for i in range(number_image):
   i = i+1
 
-  dirname = "C:/Users/xrace/Documents/Pixel/image%d"%i
+  dirname = "C:/Users/xrace/Desktop/Pack 1 -- Portraits/image%d"%i
   os.mkdir(dirname)
 
-  image_name = "C:/Users/xrace/Documents/Pixel/image%d.png"%i
+  image_name = "C:/Users/xrace/Desktop/Pack 1 -- Portraits/image%d.png"%i
   
   input = cv2.imread(image_name)
 
   # Get input size
   height, width = input.shape[:2]
 
-  a = round(400/width, 3)
+  a = round(500/width, 3)
     
   newh = int(height * a)
-  neww = 400
+  neww = 500
 
   g = 1
-  while g < 5:
+  while g < 6:
     if g == 1:
+      x = 50
+      size = "50px_image%d.png"%i
+    elif g ==2:
       x = 20
       size = "20px_image%d.png"%i
-    elif g ==2:
+    elif g == 3:
       x = 10
       size = "10px_image%d.png"%i
-    elif g == 3:
-      x = 5
-      size = "5px_image%d.png"%i
     elif g == 4 :
+      x = 4 
+      size = "4px_image%d.png"%i
+    elif g == 5 :
       x = 2 
       size = "2px_image%d.png"%i
+
 
     z = round(x/width, 3)
     
     y = int(height * z)
-    print(y)
-   
+    
 
     # Resize input to "pixelated" size
     temp = cv2.resize(input, (x, y), interpolation=cv2.INTER_LINEAR)
@@ -51,8 +54,8 @@ for i in range(number_image):
     # Initialize output image
     output = cv2.resize(temp, (neww, newh), interpolation=cv2.INTER_NEAREST)
 
-    cv2.imshow('Output', output)
+
     cv2.imwrite(os.path.join(dirname, size), output)
-    g += 1
+    g = g + 1
   cv2.imwrite(os.path.join(dirname, "image%d.png"%i), input)
   os.remove(image_name)
