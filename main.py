5e079e3ed1049d852f21d987d774755ad26a30b9
Tree <56370416+Tojo-hung@users.noreply.github.com>
2021-11-18 22:04:21 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import cv2
 import os
+from os import listdir, name
+from os.path import isfile, join
 
 dir = "C:/Users/xrace/Desktop/Pack 1 -- Portraits"
 
 list = os.listdir(dir) # dir is your directory path
 number_image = len(list)
 
-for i in range(number_image):
-  i = i+1
 
-  dirname = "C:/Users/xrace/Desktop/Pack 1 -- Portraits/image%d"%i
+onlyfiles = [f for f in listdir(dir) if isfile(join(dir, f))]
+
+print(onlyfiles)
+
+for i in range(number_image):
+
+  author = onlyfiles[i]
+
+
+
+  dirname = "C:/Users/xrace/Desktop/Pack 1 -- Portraits/%s"%author
   os.mkdir(dirname)
 
-  image_name = "C:/Users/xrace/Desktop/Pack 1 -- Portraits/image%d.png"%i
+  image_name = "C:/Users/xrace/Desktop/Pack 1 -- Portraits/%s.png"%author
   
   input = cv2.imread(image_name)
 
@@ -25,28 +35,32 @@ for i in range(number_image):
   neww = 500
 
   g = 1
-  while g < 6:
+  while g < 7:
     if g == 1:
       x = 50
-      size = "50px_image%d.png"%i
+      size = "50px_%s.png"%author
     elif g ==2:
       x = 20
-      size = "20px_image%d.png"%i
+      size = "20px_%s.png"%author
     elif g == 3:
       x = 10
-      size = "10px_image%d.png"%i
+      size = "10px_%s.png"%author
     elif g == 4 :
       x = 4 
-      size = "4px_image%d.png"%i
+      size = "4px_%s.png"%author
     elif g == 5 :
       x = 2 
-      size = "2px_image%d.png"%i
+      size = "2px_%s.png"%author
 
 
     z = round(x/width, 3)
     
     y = int(height * z)
-    
+
+    if g == 6 :
+      x = 1 
+      y = 1 
+      size = "1px_%s.png" %author
 
     # Resize input to "pixelated" size
     temp = cv2.resize(input, (x, y), interpolation=cv2.INTER_LINEAR)
@@ -57,5 +71,5 @@ for i in range(number_image):
 
     cv2.imwrite(os.path.join(dirname, size), output)
     g = g + 1
-  cv2.imwrite(os.path.join(dirname, "image%d.png"%i), input)
+  cv2.imwrite(os.path.join(dirname, "%s.png"%author), input)
   os.remove(image_name)
